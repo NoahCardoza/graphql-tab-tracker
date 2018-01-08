ee2b4a9acbfef49e2baf2ed7669327a06c600912
Noah Cardoza <noahcardoza@gmail.com>
2018-01-08 10:48:25 -0800

    init graphql express

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,33 @@
-const express = require('express')
+const app = require('express')()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const { sequelize } = require('./models');
 const config = require('./config/config')
-
-const app = express()
+const IsAuthenticated = require('./policies/IsAuthenticated');
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
+require('./passport')
+
+// GrpahQL dependencies
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema');
+const context = require('./graphqlContext');
+
+app.use('/graphql',
+  IsAuthenticated.soft,
+  graphqlHTTP(req => {
+    return {
+      schema,
+      context: context(req.user),
+      rootValue: req.user,
+      graphiql: true
+    }
+  }
+))
 
 app.use((req, res, next) => {
   // Easy error handling
@@ -33,7 +51,6 @@ app.use((req, res, next) => {
   next()
 })
 
-require('./passport')
 require('./routes')(app)
 
 // { force: true }
